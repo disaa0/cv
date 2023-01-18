1b35374a2795d431fa21bca7a0e862464030299c
San <disaa@protonmail.com>
2023-01-18 04:10:03 -0700

    Rework

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -9,51 +9,52 @@ module.exports = {
     'LinkedIn': `<a href="https://www.linkedin.com/in/sancdev/">
                   <i class="fa-brands fa-linkedin fact-icon"></i>sancdev
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/disaan_"><i class="fa-brands fa-twitter fact-icon"></i>disaan_</a>`,
+    //'Twitter': `<a href="https://twitter.com/disaan_"><i class="fa-brands fa-twitter fact-icon"></i>disaan_</a>`,
     'Email': '<a href="mailto:disaan@protonmail.com"><i class="fa fa-envelope fact-icon"></i>disaan@protonmail.com</a>'
   },
   skills: [
-    ['Linux', 85],
-    ['Shell', 75],
-    ['Python', 75],
-    ['Java', 60],
-    ['C++', 60],
-    ['SQL', 60],
-    ['Networking', 50],
+    ['Bash', 75],
+    ['Python', 70],
+    ['JavaScript', 70],
+    ['SQL', 70],
     ['Docker', 50],
+    ['Java', 50],
+    ['C++', 50],
     ['HTML', 80],
     ['CSS', 60],
-    ['PHP', 25],
-    ['JavaScript', 25],
+    ['PHP', 40],
   ],
   positions: [
-    {
-      title: 'Lead developer - UniSon Online',
-      period: 'Sep 2022 - ongoing',
-      skills: ['PHP', 'JavaScript', 'CSS', 'MySQL', 'School Project'],
-      git: '',
-      contents: `
-      I'm currently working UniSon Online, an University of Sonora private social network platform.
-`
-    },
-    {
-      title: 'Backend Developer - PetCenter',
-      period: 'Mar 2021 – May 2021',
-      skills: ['Python', 'Flask', 'Bootstrap5', 'School Project'],
-      git: `<a href="https://github.com/SanUnison/PetCenter">
-      <i class="fa-brands fa-github"></i>
-    </a>`,
-      contents: `
-I worked on all of the backend of PetCenter, a website used to manage all of the functions of a veterinary.
-`
-    },
   ],
   education: [
     {
       degree: 'Information and Communications Systems Engineering',
       period: 'Aug 2020 - ongoing',
       university: 'University of Sonora',
-    }
+      projects: [
+        {
+          title: 'UniSon Online',
+          period: 'Sep 2022 - ongoing',
+          skills: ['PHP', 'JavaScript', 'CSS', 'MySQL'],
+          git: '',
+          contents: `
+          University of Sonora private social network platform.
+          `
+        },
+        {
+          title: 'PetCenter',
+          period: 'Mar 2021 – May 2021',
+          skills: ['Python', 'Flask', 'Bootstrap5', 'School Project'],
+          git: `<a href="https://github.com/SanUnison/PetCenter">
+          <i class="fa-brands fa-github"></i>
+          </a>`,
+          contents: `
+          I worked on all of the backend of PetCenter, a website used to manage all of the functions of a veterinary.
+          `
+        },
+
+      ],
+    },
   ],
   languages: [
     {
