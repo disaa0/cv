147786cdb7a8ad1e2119024d511b1ccf73981b47
San <disaa@protonmail.com>
2023-02-24 23:12:50 -0700

    CV update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -9,17 +9,16 @@ module.exports = {
     'LinkedIn': `<a href="https://www.linkedin.com/in/sancdev/">
                   <i class="fa-brands fa-linkedin fact-icon"></i>sancdev
                 </a>`,
-    //'Twitter': `<a href="https://twitter.com/disaan_"><i class="fa-brands fa-twitter fact-icon"></i>disaan_</a>`,
+    //'Twitter': `<a href="https://twitter.com/"><i class="fa-brands fa-twitter fact-icon"></i></a>`,
     'Email': '<a href="mailto:disaan@protonmail.com"><i class="fa fa-envelope fact-icon"></i>disaan@protonmail.com</a>'
   },
   skills: [
-    ['Bash', 75],
-    ['Python', 70],
-    ['JavaScript', 70],
-    ['SQL', 70],
-    ['Docker', 50],
+    ['Bash', 60],
+    ['Python', 60],
+    ['JavaScript', 60],
+    ['MySQL', 60],
     ['Java', 50],
-    ['C++', 50],
+    ['C++', 35],
     ['HTML', 80],
     ['CSS', 60],
     ['PHP', 40],
@@ -33,23 +32,35 @@ module.exports = {
       university: 'University of Sonora',
       projects: [
         {
+          title: 'Notarius',
+          position: 'QA and Deployment',
+          period: 'Sep 2022 - December 2022',
+          skills: ['Python', 'Qt', 'Waterfall'],
+          git: '',
+          contents: `
+          I'm currently working as QA and Deployment developer of Notarius, a Notary management software. 
+          `
+        },
+        {
           title: 'UniSon Online',
-          period: 'Sep 2022 - ongoing',
+          position: 'Lead developer',
+          period: 'Sep 2022 - December 2022',
           skills: ['PHP', 'JavaScript', 'CSS', 'MySQL'],
           git: '<a href="https://github.com/disaa0/unison_online"> <i class="fa-brands fa-github"></i> </a>',
           contents: `
-          University of Sonora private social network platform.
+          I worked on the deployment and implementation of an hypothetical University of Sonora private social network platform.
           `
         },
         {
           title: 'PetCenter',
+          position: 'Lead developer',
           period: 'Mar 2021 – May 2021',
-          skills: ['Python', 'Flask', 'Bootstrap5', 'School Project'],
+          skills: ['Python', 'Flask', 'Bootstrap5'],
           git: `<a href="https://github.com/SanUnison/PetCenter">
           <i class="fa-brands fa-github"></i>
           </a>`,
           contents: `
-          I worked on all of the backend of PetCenter, a website used to manage all of the functions of a veterinary.
+          I lead the development of PetCenter, a website that manages all of the functions of a veterinary.
           `
         },
 
@@ -64,16 +75,60 @@ module.exports = {
     },
     {
       title: 'English',
-      level: 'Advanced C1',
-      tecnical: 'C1'
+      level: 'Fluent',
+      tecnical: ''
     },
   ],
   experience: [
     {
-      header: 'UniSon Online',
-      skills: ['Web', 'School Project'],
+      header: 'Operating systems',
+      skills: ['Linux', 'Unix'],
       contents: `
-Lead developer of 'UniSon Online' an University of Sonora Private Social Network Platform Demostration based HumHub.
+Deep knowledge of Linux operating systems.
+`
+    },
+  ],
+  additionalSkills: [
+    {
+      content: `
+Strong problem-solving skills and ability to work in a team environment.
+`
+    },
+    {
+      content: `
+Experience with agile and waterfall software development methodologies.
+`
+    },
+    {
+      content: `
+Familiarity with containerization technologies such as Docker.
+`
+    },
+    {
+      content: `
+Experience with version control tools and platforms.
+`
+    },
+    {
+      content: `
+Deep knowledge of Linux operating systems.
+`
+    },
+    {
+      content: `
+Familiarity with relational Database Management Systems such as MySQL, PostgreSQL and SQL Server.
+`
+    },
+    {
+      content: `
+      Ability to write clean, maintainable, and efficient code.
+`
+    },
+  ],
+additionalExperience: [
+    {
+      content: `
+.
 `
     },
   ]
